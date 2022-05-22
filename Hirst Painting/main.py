82199e20a46a891f56bcf0a8b9b48e41cbb79e69
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-05-22 10:54:29 -0500

    Add files via upload

--- Hirst Painting/main.py
+++ Hirst Painting/main.py
@@ -1,11 +1,24 @@
 from turtle import Turtle, Screen
+from random import randint
 
-timmy = Turtle()
-timmy.shape("classic")
-timmy.color("purple3")
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
-
+turtle = Turtle()
+turtle.shape("classic")
+# turtle.color("purple3")
+shape_sides = 3
 screen = Screen()
+screen.colormode(255)
+
+for _ in range(8):
+    turtle.color(randint(0, 255),
+          randint(0, 255),
+          randint(0, 255))
+    degrees = 360 / shape_sides
+    times_to_repeat = shape_sides
+    while times_to_repeat > 0:
+        turtle.forward(100)
+        turtle.right(degrees)
+        times_to_repeat -= 1
+    shape_sides += 1
+
+
 screen.exitonclick()
